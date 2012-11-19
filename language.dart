88359562c5ddb3ff564237933968a0f8f0dfc8d8
OutOfBrain <outofbrain@gmail.com>
2012-11-19 23:36:30 +0100

    direct constructor initialization and default values

--- language.dart
+++ language.dart
@@ -1,3 +1,28 @@
+/**
+ * constructor initialization
+ */
+class ConstructorInitializationDefault {
+  num a, b, c;
+
+  // b gets the value null, a and c get initialized directly
+  ConstructorInitializationDefault(this.a, b, this.c);
+}
+
+class ConstructoreInitializationOverwrite {
+  num a, b, c;
+
+  ConstructoreInitializationOverwrite(this.a, this.b, c) {
+    // a gets overwritten with whatever was passed to b as a param. c stays null
+    this.a = this.b;
+  }
+}
+
+class NamedConstructor {
+  num a, b;
+
+  NamedConstructor(this.a, this.b); // normal direct init constructor
+}
+
 void main() {
-  print("Hello, World!");
+
 }
